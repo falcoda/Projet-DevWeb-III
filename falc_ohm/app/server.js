fd4fdcb1d4bda1b6c801971e491800785a987b5f
francois-charlier <64131518+francois-charlier@users.noreply.github.com>
2021-03-21 17:43:20 +0100

    Francois
    Modification de l'arboresence

---
+++ falc_ohm/app/server.js
@@ -0,0 +1,29 @@
+let express = require("express");
+let app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (request, response)=> {
+    response.render("pages/accueil")
+});
+
+app.get("/contact", (request, response)=> {
+    response.render("pages/contact")
+});
+
+app.get("/materiel", (request, response)=> {
+    response.render("pages/materiel")
+});
+
+app.get("/devis", (request, response)=> {
+    response.render("pages/devis")
+});
+
+app.get("/connexion", (request, response)=> {
+    response.render("pages/connexion")
+});
+
+
+app.listen(83);
