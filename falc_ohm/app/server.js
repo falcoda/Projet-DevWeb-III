f6f4b475b8f452a10b03f64cba4bc580b79fd583
francois-charlier <64131518+francois-charlier@users.noreply.github.com>
2021-04-21 11:22:49 +0200

    Francois
    début du responsive de la page matériel

--- falc_ohm/app/server.js
+++ falc_ohm/app/server.js
@@ -3,11 +3,11 @@ let nodemailer = require("nodemailer");
 let express = require("express");
 let cors = require("cors");
 let app = express();
-
+//<<<<<<< HEAD
 //let bootstrap = require('bootstrap');
 
 /*const bootstrap = require('bootstrap');
-
+>>>>>>> 86bb03ddd7877e17cedca3d0eadb0465ea7e9483*/
 
 function validateEmail(email) {
 	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -50,52 +50,43 @@ app.post("/mail", (request, response)=>{
 		text: request.body.commentaire
 	};
 
-	if(request.body.commentaire ===""){
-	    response.send("comInvalid");
-	}
-	else if(request.body.nom ==="" ) {
-		response.send("nomInvalid");
 
 	let reponses = Object.entries(request.body);
-	let isValid ="";
-	if (!validateEmail(request.body.mail)){
-		isValid+= "-mailInvalid";
+	let isValid = "";
+	if (!validateEmail(request.body.mail)) {
+		isValid += "-mailInvalid";
 
 	}
-	for(let i of reponses){
-		if (i[1] === ""  ){
-			if (i[0]=== "nom"){
-				isValid+= "-nomInvalid";
-			}
-			else if (i[0]=== "prenom"){
-				isValid+= "-prenomInvalid";
-			}
-			else{
-				isValid+= "-comInvalid";
+	for (let i of reponses) {
+		if (i[1] === "") {
+			if (i[0] === "nom") {
+				isValid += "-nomInvalid";
+			} else if (i[0] === "prenom") {
+				isValid += "-prenomInvalid";
+			} else {
+				isValid += "-comInvalid";
 			}
 		}
 	}
 
 
-	if (request.body.commentaire !== "" && request.body.nom !== "" &&request.body.prenom !== "" && validateEmail(request.body.mail)) {
+	if (request.body.commentaire !== "" && request.body.nom !== "" && request.body.prenom !== "" && validateEmail(request.body.mail)) {
 		transporter.sendMail(mailOptions);
 		let con = mysql.createConnection({
 			host: "localhost",
 			user: "root",
 			password: "root",
-			database : "falcohm"
+			database: "falcohm"
 		});
-		con.connect(function(err) {
+		con.connect(function (err) {
 			if (err) throw err;
-				con.query("INSERT INTO messages (identite, mail, message) VALUES (?, ?, ?)", [request.body.nom +" " +request.body.prenom, request.body.mail, request.body.commentaire], function (err, result) {
+			con.query("INSERT INTO messages (identite, mail, message) VALUES (?, ?, ?)", [request.body.nom + " " + request.body.prenom, request.body.mail, request.body.commentaire], function (err, result) {
 			});
 		});
 		response.send("success");
-	}
-	else{
+	} else {
 		response.send(isValid);
 	}
-
 });
 
 app.get("/materiel/:format?", (request, response)=> {
@@ -128,6 +119,10 @@ app.get("/authentification", (request, response)=> {
 	response.render("pages/authentification");
 });
 
+app.get("/profil", (request, response)=> {
+	response.render("pages/profil");
+});
+
 app.get("/utilisateurs", (request, response)=> {
 	let con = mysql.createConnection({
 		host: "localhost",
@@ -153,6 +148,21 @@ app.post("/inscription", (request, response)=> {
 	con.connect(function(err) {
 		if (err) throw err;
 		con.query("INSERT INTO utilisateurs (nom, prenom, numerotelephone, adressemail, motdepasse) VALUES (?, ?, ?, ?, ?)", [request.body.nom, request.body.prenom, request.body.numerotel, request.body.adressemail2, request.body.motdepasse2], function (err, result) {
+			response.send("succes");
+		});
+	});
+});
+
+app.post("/ajouterMateriel", (request, response)=> {
+	let con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "root",
+		database : "falcohm"
+	});
+	con.connect(function(err) {
+		if (err) throw err;
+		con.query("INSERT INTO materiels (nom, id_categorie, prix, nombre, description) VALUES (?, ?, ?, ?, ?)", [request.body.nom, request.body.categorie, request.body.prix, request.body.nombre, request.body.description], function (err, result) {
 			response.send("succes");
 		});
 	});
