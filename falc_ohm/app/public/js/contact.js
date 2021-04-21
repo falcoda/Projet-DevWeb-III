039a32f3b5ef44fdbe443b99b3024c7e986c579e
CorentinDallenogare <he201885@students.ephec.be>
2021-04-21 10:46:03 +0200

    map

--- falc_ohm/app/public/js/contact.js
+++ falc_ohm/app/public/js/contact.js
@@ -125,3 +125,20 @@ class FormsPage extends React.Component {
 
 ReactDOM.render(<FormsPage/>,document.getElementById("FormsPage"));
 
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox/streets-v11',
+	tileSize: 512,
+	zoomOffset: -1,
+	accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+	color: 'red',
+	fillColor: '#f03',
+	fillOpacity: 0.5,
+	radius: 500
+}).addTo(mymap);
