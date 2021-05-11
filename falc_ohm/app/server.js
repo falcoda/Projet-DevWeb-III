b33ff6c2ee0e8708c1f4569b63248ecf247f7ff2
MaximeLitsEphec <he201658@students.ephec.be>
2021-05-11 20:12:03 +0100

    Maxime
    Retrait de quelques lignes en double

--- falc_ohm/app/server.js
+++ falc_ohm/app/server.js
@@ -256,51 +256,6 @@ app.get("/administration", (request, response)=> {
 	response.render("pages/administration");
 });
 
-
-app.get("/liste_utilisateur", (request, response)=> {
-	let con = mysql.createConnection({
-		host: "localhost",
-		user: "root",
-		password: "root",
-		database : "falcohm"
-	});
-	con.connect(function(err) {
-		if (err) throw err;
-		con.query("SELECT utilisateurs.nom, utilisateurs.prenom, utilisateurs.numerotelephone as numero, utilisateurs.adressemail as mail from utilisateurs", function (err, result) {
-			response.send(JSON.stringify(result));
-		});
-	});
-});
-
-
-app.post("/nombre_materiel", (request, response)=> {
-	let con = mysql.createConnection({
-		host: "localhost",
-		user: "root",
-		password: "root",
-		database : "falcohm"
-	});
-
-	con.connect(function(err) {
-		if (err) throw err;
-		con.query("SELECT m.nom,m.nombre from materiels as m ", function (err, result) {
-			result.forEach(function(item){
-
-				if(item.nombre !== Number(request.body[item.nom]) && request.body[item.nom] !== undefined){
-
-					con.query("UPDATE materiels SET nombre = '"+Number(request.body[item.nom])+"' WHERE nom ='" + item.nom+"'"
-					);
-				}
-			});
-
-		});
-
-
-
-
-	});
-});
-
 app.get("/commande", (request, response)=> {
 	let con = mysql.createConnection({
 		host: "localhost",
